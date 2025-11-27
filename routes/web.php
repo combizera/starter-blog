cb8e1c7c2e3167ac3216b6419d26150b1b998e34
combizera <ycombi349@gmail.com>
2025-11-27 19:58:05 -0300

    feat(routes): add route for blog categories index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog/categories', [CategoryController::class, 'index'])->name('blog.categories.index');
