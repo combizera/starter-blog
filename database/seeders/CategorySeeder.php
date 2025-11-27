9b4d0f5954c645a10678755dc92d8d85a4f95f21
combizera <ycombi349@gmail.com>
2025-11-27 19:28:57 -0300

    feat(Category): create Model/factory/seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->word();
+
+        return [
+            'name' => $name,
+            'slug' => \Str::slug($name),
+        ];
+    }
+}
