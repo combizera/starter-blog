6f44dd55960c7964eb6e657744432b309740ff70
combizera <ycombi349@gmail.com>
2025-11-28 12:14:33 -0300

    feat(categories): add create category form and controller method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,6 +19,14 @@ class CategoryController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    /**
      * Store a newly created resource in storage.
      */
     public function store(CategoryRequest $request)
---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,45 @@
+<x-layout>
+
+  <h1 class="text-xl font-semibold mb-4 text-zinc-300 border-b border-zinc-700 pb-4">
+    Create New Category
+  </h1>
+
+  <x-nav.categories />
+
+  <form action="{{ route('categories.store') }}" method="POST" class="grid grid-cols-2 gap-4">
+    @csrf
+
+    <div class="flex">
+      <x-input
+        label="Name"
+        placeholder="Category Name"
+        name="name"
+        required
+      />
+      @error('name')
+        <div class="text-red-600 mt-1">
+          {{ $message }}
+        </div>
+      @enderror
+    </div>
+
+    <div>
+      <x-input
+        label="Slug"
+        placeholder="category-slug"
+        name="slug"
+        required
+      />
+      @error('slug')
+        <div class="text-red-600 mt-1">
+          {{ $message }}
+        </div>
+      @enderror
+    </div>
+    <div class="col-span-2">
+      <x-button class="w-full font-bold" outline zinc lg flat right-icon="plus" type="submit">
+        Create Category
+      </x-button>
+    </div>
+  </form>
+</x-layout>
