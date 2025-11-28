c188b2d61f38aeff7fad7922dca7df87d13fea18
combizera <ycombi349@gmail.com>
2025-11-28 18:48:42 -0300

    feat(PostController): create resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\PostController;
+    use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('site.index');
 
 Route::resource('/blog/categories', CategoryController::class)->except('show');
+Route::resource('/blog/posts', PostController::class)->except('show');
